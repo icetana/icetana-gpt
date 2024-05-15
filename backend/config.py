4cf896a58d3ffee76381bed059e117d6faa0836c
anthony-icetana <62584182+anthony-icetana@users.noreply.github.com>
2024-05-15 11:06:09 +0800

    Rebrand Open WebUI to icetanaGPT (#2)

--- backend/config.py
+++ backend/config.py
@@ -72,9 +72,7 @@ for source in log_sources:
 log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
 
-WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
-if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
+WEBUI_NAME = os.environ.get("WEBUI_NAME", "icetanaGPT")
 
 WEBUI_URL = os.environ.get("WEBUI_URL", "http://localhost:3000")
 
